db5657e124f439bdb0d70974d4204bd87e4274d3
Novak <novak@debian.com>
2024-12-23 03:05:21 +0100

    update in term_util lib - Makefile created.

---
+++ lib/term-util/src/ansi_op.c
@@ -0,0 +1,10 @@
+#include "ansi_op.h"
+
+void rel_move_cursor(int row_diff, int col_diff);
+void rel_move_cursor_y(int row_diff);
+void rel_move_cursor_x(int col_diff);
+
+void abs_reset_cursor();
+void abs_move_cursor_x(int col_diff);
+void abs_move_cursor_y(int row_diff);
+void abs_move_cursor(size_t row, size_t col);
